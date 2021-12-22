ce52b772e9901bd8c3c6c3bfa97d5ed38173590d
Grizz <1985661975@qq.com>
2021-12-22 11:09:15 +0800

    init commit

---
+++ cnn.py
@@ -0,0 +1,175 @@
+import time
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import ssl
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+# Assuming that we are on a CUDA machine, this should print a CUDA device:
+print(device)
+
+# load cifar-10 data set
+ssl._create_default_https_context = ssl._create_unverified_context
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 5
+
+train_set = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                         download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                           shuffle=True, num_workers=0)
+
+test_set = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                        download=True, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
+                                          shuffle=False, num_workers=0)
+print(len(train_set))
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# show some of the training images
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def img_show(img):
+    img = img / 2 + 0.5  # un-normalize
+    np_img = img.numpy()
+    plt.imshow(np.transpose(np_img, (1, 2, 0)))
+    plt.show()
+
+
+# get some random training images
+data_iter = iter(train_loader)
+images, labels = data_iter.next()
+
+# show images
+img_show(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join('%5s' % classes[labels[j]] for j in range(batch_size)))
+
+# Convolutional Neural Network
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)  # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+# use PU
+net.to(device)
+
+# Loss Function
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+# train
+time1 = time.time()
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        # use GPU
+        inputs, labels = data[0].to(device), data[1].to(device)
+        # use CPU
+        # inputs, labels = data
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:  # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+time2 = time.time()
+print(time2-time1)
+
+# save the trained model
+PATH = './cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+# test the model on test set
+data_iter = iter(test_loader)
+images, labels = data_iter.next()
+
+# print images
+img_show(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# load back the trained mdoel
+net = Net()
+net.load_state_dict(torch.load(PATH))
+# output
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+# correctness
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in test_loader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+        100 * correct / total))
+
+# prepare to count predictions for each class
+correct_pred = {classname: 0 for classname in classes}
+total_pred = {classname: 0 for classname in classes}
+
+# again no gradients needed
+with torch.no_grad():
+    for data in test_loader:
+        images, labels = data
+        outputs = net(images)
+        _, predictions = torch.max(outputs, 1)
+        # collect the correct predictions for each class
+        for label, prediction in zip(labels, predictions):
+            if label == prediction:
+                correct_pred[classes[label]] += 1
+            total_pred[classes[label]] += 1
+
+# print accuracy for each class
+for classname, correct_count in correct_pred.items():
+    accuracy = 100 * float(correct_count) / total_pred[classname]
+    print("Accuracy for class {:5s} is: {:.1f} %".format(classname, accuracy))
